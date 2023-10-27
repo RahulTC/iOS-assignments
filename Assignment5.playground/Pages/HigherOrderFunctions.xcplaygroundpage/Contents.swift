41bb0125e68bd6f093fb80ce2dc5a0e00ffcb0cf
RahulTC <rahul.adepu@techconsulting.tech>
2023-10-27 16:55:30 -0400

    updated assignment 5

--- Assignment5.playground/Pages/HigherOrderFunctions.xcplaygroundpage/Contents.swift
+++ Assignment5.playground/Pages/HigherOrderFunctions.xcplaygroundpage/Contents.swift
@@ -13,6 +13,11 @@ struct Pokemon{
     let pokemonNPN: Int
 }
 
+struct Pokemon2Darray{
+    let pokemonName: String
+    let pokemonType: [String]
+}
+
 let pokemons = [Pokemon(pokemonName: "Pikachu", pokemonNPN: 25),
                 Pokemon(pokemonName: "Charizard", pokemonNPN: 6),
                 Pokemon(pokemonName: "Greninja", pokemonNPN: 658),
@@ -21,40 +26,66 @@ let pokemons = [Pokemon(pokemonName: "Pikachu", pokemonNPN: 25),
                 Pokemon(pokemonName: "Infernape", pokemonNPN: 392)]
 
 // MARK: 1. Map
+print("-------------------------MAP-------------------------")
 // It takes a collection and applies a function to each element and returns a new collection with results.
 var pokemonNamesInCaps = pokemons.map{ $0.pokemonName.uppercased() }
 print("pokemon Names ->",pokemonNamesInCaps)
 
 // MARK: 2. Filter
+print("-------------------------Filter-------------------------")
 // It takes a collection and applies a function to it and then returns
 var evenNumber = pokemons.filter{$0.pokemonNPN % 2 == 0}
-print("Even Numbers pokemons ->",evenNumber)
+print("Even Numbers pokemons ->",evenNumber.map{$0.pokemonName})
 
 // MARK: 3. Reduce
+print("-------------------------Reduce-------------------------")
 var allPokemonNPN = pokemons.map{ $0.pokemonNPN }
 var sumOfAll = allPokemonNPN.reduce(0,+)
 print("Sum of all ->", sumOfAll)
 
 // MARK: 4. Sort
+print("-------------------------Sort-------------------------")
 var sortedPokemonAlphabetically = pokemons.sorted { poke1, poke2 in
     poke1.pokemonName < poke2.pokemonName
 }
-print("Sorted ascending order of numbers ->", sortedPokemonAlphabetically)
+print("Sorted ascending order of numbers ->", sortedPokemonAlphabetically.map{$0.pokemonName})
 
 // MARK: 5. Flat Map
-let array2d = [[123, 55], [0, 0], [1, 4], [6, 55]]
-print("Flat map on duplicated nil values",array2d.flatMap{$0})
+print("-------------------------Flat Map-------------------------")
+let pokemon2dArray = [Pokemon2Darray(pokemonName: "Pikachu", pokemonType: ["Electric"]),
+                      Pokemon2Darray(pokemonName: "Charizard", pokemonType: ["Fire", "Flying"]),
+                      Pokemon2Darray(pokemonName: "Greninja", pokemonType: ["Water", "Dark"]),
+                      Pokemon2Darray(pokemonName: "Sceptile", pokemonType: ["Grass"]),
+                      Pokemon2Darray(pokemonName: "Lucario", pokemonType: ["Flighting", "Steel"]),
+                      Pokemon2Darray(pokemonName: "Infernape", pokemonType: ["Fire", "Fighting"])]
+let arrayVal = pokemon2dArray.flatMap{$0.pokemonType}
+print("Flat map on 2d Array values",pokemon2dArray.flatMap{$0.pokemonType})
+
+//// Use flatMap to create a new array with one Pokemon type per name
+//let flattenedPokemonArray = pokemon2dArray.flatMap { pokemon in
+//    pokemon.pokemonType.map { type in
+//        Pokemon2Darray(pokemonName: pokemon.pokemonName, pokemonType: [type])
+//    }
+//}
+//
+//// Printing the result
+//for pokemon in flattenedPokemonArray {
+//    print("Pokemon Name: \(pokemon.pokemonName), Pokemon Type: \(pokemon.pokemonType)")
+//}
+
+
+
 
 // MARK: 6. Compact Map
+print("-------------------------Compact Map-------------------------")
 let duplicatedNilArray = [123, 55, nil, 0, 0, 1, 4, 6, 55]
 print("Compact map on 2D array",duplicatedNilArray.compactMap({ $0 }))
 
 // MARK: 7. For Each
+print("-------------------------For Each-------------------------")
 pokemons.forEach { print($0) }
 
 // MARK: 8. Zip
-let pokemons2 = [Pokemon(pokemonName: "Blastoise", pokemonNPN: 9),
-                Pokemon(pokemonName: "Pidgeot", pokemonNPN: 18)]
-
-let zipPokemons = zip(pokemons,pokemons2)
+print("-------------------------Zip-------------------------")
+let zipPokemons = zip(pokemons,pokemon2dArray)
 print(zipPokemons)
